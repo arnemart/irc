fe650f8d6b39b8e4cd7220179299f1f5cb9180d5
Arne Martin Aurlien <am@aurlien.net>
2023-05-28 12:44:09 +0200

    yolo

--- server.js
+++ server.js
@@ -21,13 +21,65 @@ const server = https.createServer(options, handler)
 const { WebSocketServer, OPEN } = require("ws")
 const wss = new WebSocketServer({ server })
 
+const users = new Map()
+
+const encodeMsg = (msg) => {
+  const now = new Date()
+  msg.time = `${now.getHours() < 10 ? "0" : ""}${now.getHours()}:${
+    now.getMinutes() < 10 ? "0" : ""
+  }${now.getMinutes()}`
+  return JSON.stringify(msg)
+}
+
+const broadcast = (msg) => {
+  const data = encodeMsg(msg)
+  for (client of users.keys()) {
+    if (client.readyState == OPEN) {
+      client.send(data, { binary: false })
+    }
+  }
+}
+
 wss.on("connection", (ws) => {
+  users.set(ws, null)
+
+  const leave = () => {
+    const nick = users.get(ws)
+    if (nick != null) {
+      broadcast({ type: "leave", nick })
+    }
+    users.delete(ws)
+  }
+
+  ws.on("error", leave)
+  ws.on("close", leave)
+
   ws.on("message", (data) => {
-    wss.clients.forEach((client) => {
-      if (client.readyState == OPEN) {
-        client.send(data, { binary: false })
+    data = data.toString()
+    const nick = users.get(ws)
+    if (/^\/nick/.test(data)) {
+      const newnick = data.replace(/^\/nick\s*/, "")
+      if (!/^[\wæøåÆØÅäÂöÖ\.@_-]{2,20}$/.test(newnick)) {
+        ws.send(encodeMsg({ type: "error", msg: "Invalid nick" }))
+      } else if (new Set(users.values()).has(newnick)) {
+        ws.send(encodeMsg({ type: "error", msg: "Nick already in use" }))
+      } else {
+        users.set(ws, newnick)
+        if (nick == null) {
+          ws.send(encodeMsg({ type: "welcome" }))
+          broadcast({ type: "join", nick: newnick, oldnick: nick })
+        } else {
+          broadcast({ type: "nick", nick: newnick, oldnick: nick })
+        }
       }
-    })
+    } else if (/^\/me/.test(data)) {
+      const msg = data.replace(/^\/me\s*/, "")
+      if (msg != "") {
+        broadcast({ type: "me", nick, msg })
+      }
+    } else if (data != "") {
+      broadcast({ type: "msg", nick, msg: data })
+    }
   })
 })
 
